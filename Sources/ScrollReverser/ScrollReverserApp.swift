c5ae325fdd2e5800ad695b9b5022ee1a9b03826b
Oscar Chen <oscar1149412@gmail.com>
2026-05-10 00:50:52 -0500

    scroll app Scene

--- Sources/ScrollReverser/ScrollReverserApp.swift
+++ Sources/ScrollReverser/ScrollReverserApp.swift
@@ -1,1 +1,20 @@
-print("ScrollReverser starting")
+import SwiftUI 
+
+@main
+struct ScrollReverserApp: App {
+        @State private var isEnabled = true 
+
+        var body: some Scene {
+                MenuBarExtra("ScrollReverser", systemImage: "arrow.up.arrow.down") {
+                        Toggle("Enabled", isOn: $isEnabled)
+                        Divider()
+                        Button("Open Settings") {
+                                // TODO: 
+                        }
+                        Divider()
+                        Button("Quit") {
+                                NSApplication.shared.terminate(nil)
+                            }
+                    }
+            }
+    }
